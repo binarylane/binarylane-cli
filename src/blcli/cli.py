9960b26bbb87fbb30015845cb3201f2b0b052534
Nathan O'Sullivan <nathan.osullivan@mammoth.com.au>
2022-09-15 10:24:34 +1000

    feat: add support for path parameters

--- src/blcli/cli.py
+++ src/blcli/cli.py
@@ -6,23 +6,45 @@ prog_parser = argparse.ArgumentParser(
     prog="bl", description="bl is a command-line interface for the BinaryLane API"
 )
 
-subparsers = prog_parser.add_subparsers(title="Available Commands", metavar="")
+command_action: "argparse._SubParsersAction" = prog_parser.add_subparsers(
+    title="Available Commands", metavar=""
+)
 
-# parser.add_argument("--version", help="Display version", action="store_true")
 
-
-def _add_command(parser, name, help=None):
+def _add_command(
+        action: "argparse._SubParsersAction", prefix: str, name: str, help: str = None
+):
     def wrap(func):
-        command = parser.add_parser(name, help=help)
-        command.set_defaults(func=func)
-        command_parser = command.add_subparsers(title=func.__name__, metavar="")
-        func.add_command = functools.partial(_add_command, command_parser)
+        command_name = (
+            name[len(prefix) + 1:] if name.startswith(prefix + "_") else name
+        )
+
+        command_parser = action.add_parser(command_name, help=help)
+        handler = func(command_parser)
+        command_parser.set_defaults(func=handler)
         return func
 
     return wrap
 
 
-add_command = functools.partial(_add_command, subparsers)
+def _add_group(
+        action: "argparse._SubParsersAction", prefix: str, name: str, help: str = None
+):
+    def wrap(func):
+        command_name = (
+            name[len(prefix) + 1:] if name.startswith(prefix + "_") else name
+        )
+
+        group_parser = action.add_parser(command_name, help=help)
+        group_action = group_parser.add_subparsers(title=func.__name__, metavar="")
+        func.add_group = functools.partial(_add_group, group_action, command_name)
+        func.add_command = functools.partial(_add_command, group_action, command_name)
+        return func
+
+    return wrap
+
+
+add_group = functools.partial(_add_group, command_action, "")
 
 
 def run(args):
@@ -34,7 +56,22 @@ def run(args):
     )
 
     parsed = prog_parser.parse_args(args)
-    if "func" in parsed and parsed.func(client):
+
+    # Haven't reached a command
+    if not "func" in parsed:
+        prog_parser.parse_args(args + ["--help"])
         return
 
-    prog_parser.parse_args(args + ["--help"])
+    # Run the command's handler
+    handler = parsed.func
+    del parsed.func
+
+    parsed.client = client
+    response = handler(**vars(parsed))
+    if not response:
+        prog_parser.parse_args(args + ["--help"])
+        return
+
+    # display response
+    print(response)
+    return
