562bc5dc308971309b77aa50d605251f962cefe3
Nathan O'Sullivan <nathan.osullivan@mammoth.com.au>
2022-09-17 19:49:28 +1000

    chore: add pre-commit config, fix type annotations and lint errors

--- src/blcli/__init__.py
+++ src/blcli/__init__.py
@@ -1,19 +1,28 @@
+""" Command line interface for the BinaryLane API """
+import importlib
 import sys
 import time
+from typing import Any, Callable
 
 from . import cli
 
-# Currently this is what triggers all the argparse construction, so its slow:
-start = time.time()
-from . import client
 
-end = time.time()
-cli.debug(f"DEBUG: {int(1000*(end-start))}ms | from . import client")
-
-
-def main():
-
+def debug_time(func: Callable[[], Any], msg: str = None) -> None:
+    """
+    Time how long func takes to run, and write to debug log.
+    :param func: the function to time
+    :param msg: message to show in debug log next to the elapsed time
+    """
     start = time.time()
-    cli.run(sys.argv[1:])
+    func()
     end = time.time()
-    cli.debug(f"DEBUG: {int(1000*(end-start))}ms | main()")
+    cli.debug(f"DEBUG: {int(1000 * (end - start))}ms for '{msg or repr(func)}'")
+
+
+def main() -> None:
+    """
+    Entrypoint for blcli
+    """
+    # Currently this is what triggers all the argparse construction, so its slow:
+    debug_time(lambda: importlib.import_module(".client", package=__package__), "import .client")
+    debug_time(lambda: cli.run(sys.argv[1:]), "cli.run()")
--- src/blcli/cli.py
+++ src/blcli/cli.py
@@ -1,41 +1,61 @@
+""" CLI implementation """
+
 import argparse
 import functools
+import importlib
 import os
 import sys
 from enum import Enum
-from typing import List, Union
+from typing import Any, Callable, List, Optional, Sequence, Union
+
+from terminaltables import SingleTable
+
+try:
+    # FIXME: used generated code, parked here for now
+    # pylint: disable=unused-import
+    from argparse import BooleanOptionalAction  # type: ignore
+except ImportError:
+    from .actions import BooleanOptionalAction
 
 
-def debug(*args, **kwargs):
+def debug(*args: str) -> None:
+    """Wrapper for print() that only produces output when DEBUG is enabled"""
     if os.getenv("DEBUG"):
-        print(*args, **kwargs, file=sys.stderr)
+        print(*args, file=sys.stderr)
 
 
 class CommandParser(argparse.ArgumentParser):
-    def __init__(self, *args, **kwargs):
+    """Enhanced ArgumentParser with support for generic types"""
+
+    def __init__(self, **kwargs: Any):
         kwargs["formatter_class"] = lambda prog: argparse.HelpFormatter(
             prog,
             max_help_position=100,
             width=None,
         )
 
-        super().__init__(*args, **kwargs)
+        super().__init__(**kwargs)
 
-    def add_argument(self, *args, **kwargs):
+    def cli_argument(self, *args: str, **kwargs: Any) -> Optional[argparse.Action]:
+        """Add CLI argument to parser"""
+        if "description" in kwargs:
+            kwargs["help"] = kwargs["description"]
+            del kwargs["description"]
+
         _type = kwargs.get("type")
         if _type is None:
-            return super().add_argument(*args, **kwargs)
+            return self.add_argument(*args, **kwargs)
 
         dest = kwargs.get("dest", "?")
 
         # Handle unions:
         if getattr(_type, "__origin__", None) is Union:
             # delayed import to avoid circular reference
-            from .client.types import Unset
+            unset = importlib.import_module(".client.types", __package__).Unset
 
             inner_types = list(_type.__args__)
-            if Unset in inner_types:
-                inner_types.remove(Unset)
+            if unset in inner_types:
+                inner_types.remove(unset)
             if type(None) in inner_types and not kwargs.get("required", True):
                 inner_types.remove(type(None))
 
@@ -53,10 +73,8 @@ class CommandParser(argparse.ArgumentParser):
             inner_type = _type.__args__[0]
 
             if inner_type not in (int, str):
-                debug(
-                    f"WARN: ignoring {self.prog} {dest} type={_type}"
-                )
-                return
+                debug(f"WARN: ignoring {self.prog} {dest} type={_type}")
+                return None
 
             _type = inner_type
             # FIXME: Determine if 1 or 0 is required ?
@@ -65,7 +83,7 @@ class CommandParser(argparse.ArgumentParser):
         # Check we have handled all generic types:
         if getattr(_type, "__origin__", None):
             debug(f"WARN: ignoring {self.prog} {dest} type={_type}")
-            return
+            return None
 
         kwargs["type"] = _type
 
@@ -77,31 +95,28 @@ class CommandParser(argparse.ArgumentParser):
             if len(enum_options) == 1:
                 # FIXME: be better to just add to result of parse_args via orderride ?
                 self.set_defaults(**{kwargs["dest"]: enum_options[0]})
-                return
+                return None
             kwargs["choices"] = enum_options
             # print('Need to add enum!', enum_options)
 
-        super().add_argument(*args, **kwargs)
+        return self.add_argument(*args, **kwargs)
 
 
-prog_parser = CommandParser(
-    prog="bl", description="bl is a command-line interface for the BinaryLane API"
-)
+prog_parser = CommandParser(prog="bl", description="bl is a command-line interface for the BinaryLane API")
 
-command_action: "argparse._SubParsersAction" = prog_parser.add_subparsers(
-    title="Available Commands", metavar=""
-)
+AddCommandParser = Callable[..., CommandParser]
+add_command_parser: AddCommandParser = prog_parser.add_subparsers(title="Available Commands", metavar="").add_parser
+
+AddCommand = Callable[[CommandParser], None]
 
 
 def _add_command(
-        action: "argparse._SubParsersAction", prefix: str, name: str, help: str = None
-):
-    def wrap(func):
-        command_name = (
-            name[len(prefix) + 1:] if name.startswith(prefix + "_") else name
-        )
+    add_parser: AddCommandParser, prefix: str, name: str, description: str = None
+) -> Callable[[AddCommand], AddCommand]:
+    def wrap(func: AddCommand) -> AddCommand:
+        command_name = name[len(prefix) + 1 :] if name.startswith(prefix + "_") else name
 
-        command_parser = action.add_parser(command_name, help=help)
+        command_parser = add_parser(command_name, help=description)
         handler = func(command_parser)
         command_parser.set_defaults(func=handler)
         return func
@@ -109,27 +124,29 @@ def _add_command(
     return wrap
 
 
-def _add_group(
-        action: "argparse._SubParsersAction", prefix: str, name: str, help: str = None
-):
-    def wrap(func):
-        command_name = (
-            name[len(prefix) + 1:] if name.startswith(prefix + "_") else name
-        )
+AddGroup = Callable[[str, str], None]
 
-        group_parser = action.add_parser(command_name, help=help)
-        group_action = group_parser.add_subparsers(title=f"{name.title()} commands", metavar="")
-        func.add_group = functools.partial(_add_group, group_action, command_name)
-        func.add_command = functools.partial(_add_command, group_action, command_name)
+
+def _add_group(
+    add_parser: AddCommandParser, prefix: str, name: str, description: str = None
+) -> Callable[[AddGroup], AddGroup]:
+    def wrap(func: AddGroup) -> AddGroup:
+        command_name = name[len(prefix) + 1 :] if name.startswith(prefix + "_") else name
+
+        group_parser = add_parser(command_name, help=description)
+        group_action = group_parser.add_subparsers(title=f"{name.title()} commands", metavar="").add_parser
+        func.add_group = functools.partial(_add_group, group_action, command_name)  # type: ignore
+        func.add_command = functools.partial(_add_command, group_action, command_name)  # type: ignore
         return func
 
     return wrap
 
 
-add_group = functools.partial(_add_group, command_action, "")
+add_group = functools.partial(_add_group, add_command_parser, "")
 
 
-def run(args):
+def run(args: List[str]) -> None:
+    """CLI runner: parse command line input, make an API request and display the response"""
     parsed = prog_parser.parse_args(args)
 
     # Haven't reached a command
@@ -141,11 +158,11 @@ def run(args):
     handler = parsed.func
     del parsed.func
 
-    from .client import AuthenticatedClient
-
-    client = AuthenticatedClient(
+    # FIXME: avoid circular dependency by late import
+    authenticated_client = importlib.import_module(".client", __package__).AuthenticatedClient
+    client = authenticated_client(
         "https://api.binarylane.com.au",
-        open(os.path.expanduser("~/.config/python-blcli")).read().strip(),
+        get_api_token(),
     )
 
     parsed.client = client
@@ -159,7 +176,19 @@ def run(args):
     return
 
 
-def display(response):
+def get_api_token() -> str:
+    """Obtain user's API token from environment variable or configuration file"""
+    token = os.getenv("BL_CLI_TOKEN")
+    if token:
+        return token
+
+    with open(os.path.expanduser("~/.config/python-blcli"), encoding="utf-8") as config_file:
+        return config_file.read().strip()
+
+
+def display(response: Any) -> None:
+    """Display an API response as a table"""
+
     response_type = type(response)
     # print(f"-- {response_type} ({response_type.__module__}) --")
     primary = None
@@ -177,37 +206,27 @@ def display(response):
             primary_type = _type
 
     if not primary:
-        print(
-            f"ERROR: unsure how to print {response_type} ({response})", file=sys.stderr
-        )
-
-    from terminaltables import SingleTable
+        print(f"ERROR: unsure how to print {response_type} ({response})", file=sys.stderr)
+        return
 
     response = getattr(response, primary)
     if isinstance(response, list):
         header = [
-            key
-            for key in getattr(
-                primary_type, "__annotations__", {"item": "value"}
-            ).keys()
-            if key != "additional_properties"
+            key for key in getattr(primary_type, "__annotations__", {"item": "value"}) if key != "additional_properties"
         ]
-        data = [header] + [
-            flatten(item if item is str else item.to_dict().values())
-            for item in response
-        ]
+        data = [header] + [flatten(item if item is str else item.to_dict().values()) for item in response]
     elif isinstance(response, str):
         data = []
         print(response)
     else:
-        data = [["Name", "Value"]] + [
-            flatten(item, True) for item in response.to_dict().items()
-        ]
+        data = [["Name", "Value"]] + [flatten(item, True) for item in response.to_dict().items()]
     if len(data) > 1:
         print(SingleTable(data).table)
 
 
-def flatten(values, single_object: bool = False):
+def flatten(values: Sequence[Any], single_object: bool = False) -> List[str]:
+    """Transform each item in values into a format more suitable for displaying"""
+
     result: List[str] = []
     max_list = 5
     max_str = 80
@@ -221,18 +240,30 @@ def flatten(values, single_object: bool = False):
             if not single_object:
                 item = ", ".join(map(str, item))
             else:
-                item = "- " + "\n- ".join(
-                    [("  ".join(f'{key}: {value}\n' for key, value in i.items()) if type(i) is dict else str(i)) for i
-                     in item]
-                ) if item else ''
+                item = (
+                    "- "
+                    + "\n- ".join(
+                        [
+                            (
+                                "  ".join(f"{key}: {value}\n" for key, value in i.items())
+                                if isinstance(i, dict)
+                                else str(i)
+                            )
+                            for i in item
+                        ]
+                    )
+                    if item
+                    else ""
+                )
         if item_type is dict:
-            for key in ('display_name', 'name', 'slug', 'id'):
+            for key in ("display_name", "name", "slug", "id"):
                 if key in item:
                     item = item[key]
                     break
             else:
-                item = "<object>" if not single_object else "\n".join(
-                    [f'{key}: {value}' for key, value in item.items()])
+                item = (
+                    "<object>" if not single_object else "\n".join([f"{key}: {value}" for key, value in item.items()])
+                )
         if item_type is bool:
             item = "Yes" if item else "No"
 
@@ -242,55 +273,3 @@ def flatten(values, single_object: bool = False):
         result.append(item)
 
     return result
-
-
-try:
-    from argparse import BooleanOptionalAction
-except:
-    # New to 3.9
-    class BooleanOptionalAction(argparse.Action):
-        def __init__(
-                self,
-                option_strings,
-                dest,
-                default=None,
-                type=None,
-                choices=None,
-                required=False,
-                help=None,
-                metavar=None,
-        ):
-
-            _option_strings = []
-            for option_string in option_strings:
-                _option_strings.append(option_string)
-
-                if option_string.startswith("--"):
-                    option_string = "--no-" + option_string[2:]
-                    _option_strings.append(option_string)
-
-            if (
-                    help is not None
-                    and default is not None
-                    and default is not argparse.SUPPRESS
-            ):
-                help += " (default: %(default)s)"
-
-            super().__init__(
-                option_strings=_option_strings,
-                dest=dest,
-                nargs=0,
-                default=default,
-                type=type,
-                choices=choices,
-                required=required,
-                help=help,
-                metavar=metavar,
-            )
-
-        def __call__(self, parser, namespace, values, option_string=None):
-            if option_string in self.option_strings:
-                setattr(namespace, self.dest, not option_string.startswith("--no-"))
-
-        def format_usage(self):
-            return " | ".join(self.option_strings)
