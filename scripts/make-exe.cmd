e0b5d8ddde6976296ce277dc9dc8c77f8c113b4d
Nathan O'Sullivan <nathan.osullivan@mammoth.com.au>
2022-12-30 07:58:15 +0000

    ci: configure Gitlab to build Windows .exe

--- scripts/make-exe.cmd
+++ scripts/make-exe.cmd
@@ -1,14 +1,16 @@
 @echo off
 
+pip install . -t exe
+
 REM pyinstaller --paths is described as being like PYTHONPATH, but
 REM it doesnt seem to function correctly when collecting submodules
 REM so we will just set PYTHONPATH explicitly
-set PYTHONPATH=src
+set PYTHONPATH=exe
 
-echo from blcli import app ; app.main() > src\bl.py
+echo from blcli import app ; app.main() > exe\bl.py
 
 FOR /F "tokens=*" %%g IN ('poetry version --short') do (set BLCLI_VERSION=%%g)
-echo __version__ = '%BLCLI_VERSION%' > src\blcli\_version.py
+echo __version__ = '%BLCLI_VERSION%' > exe\blcli\_version.py
 
-poetry run pyinstaller -F --collect-submodules blcli src\bl.py --clean
-del src\bl.py src\blcli\_version.py
+pyinstaller -F --collect-submodules blcli exe\bl.py --clean
+rmdir /s /q exe
