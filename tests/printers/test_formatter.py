7810e5f80206dd89a4d1a2070b64d2b027632add
David Pershouse <david.pershouse@mammoth.com.au>
2023-02-07 13:44:02 +1000

    add some formatter tests

--- tests/printers/test_formatter.py
+++ tests/printers/test_formatter.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
-from typing import Any, Dict, List
+from typing import Any, Dict, List, cast
+
+from binarylane.models.problem_details import ProblemDetails
+from binarylane.models.validation_problem_details import ValidationProblemDetails
+from binarylane.models.validation_problem_details_errors import ValidationProblemDetailsErrors
 
 from binarylane.console.printers import formatter
 
@@ -52,4 +56,51 @@ def test_format_primary_str() -> None:
     assert formatter.format_response(response, True) == {
         "table": [[formatter.DEFAULT_HEADING], [response.message]],
         "title": None,
+    }
+
+
+def test_format_problem_details() -> None:
+    response = ProblemDetails(
+        title="General Protection Fault",
+        type="https://httpstatuses.com/500",
+        status=500,
+        detail="The server encountered an unexpected condition that prevented it from fulfilling the request.",
+    )
+    assert formatter.format_response(response, True) == {
+        "table": cast(List[List[str]], []),
+        "title": f"Error: {response.title}\n\n{response.detail}",
+    }
+
+
+def test_format_validation_problem_details() -> None:
+    response = ValidationProblemDetails(
+        title="There were validation errors",
+        type="https://httpstatuses.com/400",
+        status=400,
+        detail="Please fix the errors and try again",
+        errors=ValidationProblemDetailsErrors.from_dict(
+            {"name": ["Name is required", "name must be unique"], "region": ["Region is required"]}
+        ),
+    )
+    assert formatter.format_response(response, True) == {
+        "table": [
+            ["field", "errors"],
+            ["name", "Name is required\nname must be unique"],
+            ["region", "Region is required"],
+        ],
+        "title": f"Error: {response.title}",
+    }
+
+
+def test_format_validation_problem_details_no_errors() -> None:
+    response = ValidationProblemDetails(
+        title="Email already in use",
+        type="https://httpstatuses.com/400",
+        status=400,
+        detail="Please fix the errors and try again",
+        errors=None,
+    )
+    assert formatter.format_response(response, True) == {
+        "table": cast(List[List[str]], []),
+        "title": f"Error: {response.title}",
     }
