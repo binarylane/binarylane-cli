8259373c566c0ef9e4d8fba196e111f87bd57f1b
Nathan O'Sullivan <nathan.osullivan@mammoth.com.au>
2023-02-27 10:44:20 +1000

    fix name of LazyRunner.__init__ parameter

--- src/binarylane/console/app/lazy_runner.py
+++ src/binarylane/console/app/lazy_runner.py
@@ -6,16 +6,17 @@ from binarylane.console.runners import Runner
 class LazyRunner(Runner):
     """LazyRunner is a Runner that is imported ondemand by LazyLoader"""
 
-    proxy: Runner
+    # The loader provides our name and description
+    loader: Runner
 
-    def __init__(self, context: Runner) -> None:
-        super().__init__(context._context)
-        self.proxy = context
+    def __init__(self, loader: Runner) -> None:
+        super().__init__(loader._context)
+        self.loader = loader
 
     @property
     def name(self) -> str:
-        return self.proxy.name
+        return self.loader.name
 
     @property
     def description(self) -> str:
-        return self.proxy.description
+        return self.loader.description
