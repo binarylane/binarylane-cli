434ec0a477b51eb900e50efcd30b7ea50b35bf43
Nathan O'Sullivan <nathan.osullivan@mammoth.com.au>
2023-03-14 22:35:18 +1000

    Add OS, arch, and python version to user agent

--- src/binarylane/console/util.py
+++ src/binarylane/console/util.py
@@ -1,14 +1,17 @@
 from __future__ import annotations
+from platform import system, machine, python_implementation, python_version
 
 from binarylane.pycompat.importlib import metadata
 
 from binarylane.client import AuthenticatedClient
 from binarylane.config import Config
 
+DISTRIBUTION_NAME = "binarylane-cli"
+
 
 def get_version() -> str:
     try:
-        return metadata.distribution("binarylane-cli").version
+        return metadata.distribution(DISTRIBUTION_NAME).version
     except metadata.PackageNotFoundError:
         return "dev"
 
@@ -20,5 +23,9 @@ def create_client(config: Config) -> AuthenticatedClient:
         verify_ssl=not config.api_development,
         timeout=5.0,
         raise_on_unexpected_status=False,
-        headers={"User-Agent": f"binarylane-cli/{get_version()}"},
+        headers={"User-Agent": get_user_agent()},
     )
+
+
+def get_user_agent():
+    return f"{DISTRIBUTION_NAME}/{get_version()} ({system()}/{machine()}) {python_implementation()}/{python_version()}"
