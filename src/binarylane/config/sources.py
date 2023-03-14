c2e1c5565be36ed22c324386b162ee6086edd40d
Nathan O'Sullivan <nathan.osullivan@mammoth.com.au>
2023-03-14 20:25:06 +1000

    Use Path.home() instead of os.getenv("HOME")

--- src/binarylane/config/sources.py
+++ src/binarylane/config/sources.py
@@ -72,11 +72,8 @@ class FileSource:
         if xdg_config_home:
             return Path(xdg_config_home)
 
-        # Otherwise, use $HOME/.config
-        home = os.getenv("HOME")
-        if not home:
-            raise EnvironmentError("$HOME is not set?")
-        home_config = Path(home) / ".config"
+        # Otherwise, use HOME/.config
+        home_config = Path.home() / ".config"
 
         # Ensure $HOME/.config is a directory, creating it if necessary
         if home_config.exists() and not home_config.is_dir():
